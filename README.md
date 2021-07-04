efded1999e10915c5cf397fc78a672b718ed22da
gayan madurapperuma <gayanmadurapperuma@gayans-MacBook-Air.local>
2021-07-04 11:13:50 +0530

    refactoring catCard application

---
+++ README.md
@@ -0,0 +1,49 @@
+
+# Cat Card Application
+
+Refactoring existing cat card application
+
+## Running Locally
+
+Make sure you have [Node.js](https://nodejs.org/en/) installed.
+
+```bash
+git clone https://github.com/gayanMadurapperuma/catCard.git
+cd catCard
+```
+
+### Installing
+
+using [yarn](https://yarnpkg.com)
+```bash
+yarn
+``` 
+
+using npm
+```bash
+npm install
+``` 
+
+### Running
+
+```bash
+# using yarn
+yarn start
+``` 
+
+In below are listed the supported arguments of the application. Default value display inside the brackets.
+
+* greetings (Hello)
+* who (You)
+* width (400)
+* height (500)
+* color (pink)
+* size (100)
+
+```bash
+# using arguments
+yarn start --greeting 'Welcome' --who 'Cat'
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,42 @@
+const util = require('util');
+const { writeFile } = require('fs');
+const { join } = require('path');
+const axios = require('axios');
+const blend = require('@mapbox/blend');
+const argv = require('minimist')(process.argv.slice(2));
+
+const blendPromise = util.promisify(blend);
+const writeFilePromise = util.promisify(writeFile);
+
+const {
+    greeting = 'Hello',
+    who = 'You',
+    width = 400,
+    height = 500,
+    color = 'Pink',
+    size = 100,
+} = argv;
+
+const instance = axios.create({
+    baseURL: 'https://cataas.com/cat/says/',
+    responseEncoding: 'binary'
+});
+
+const catCard = async () => {
+    try {
+        const [firstImage, secondImage] = await Promise.all([
+            instance.get(`${greeting}?width=${width}&height=${height}&color${color}&s=${size}`),
+            instance.get(`${who}?width=${width}&height=${height}&color${color}&s=${size}`),
+        ]);
+        const result = await blendPromise([
+            { buffer: Buffer.from(firstImage.data, 'binary'), x: 0, y: 0 }, 
+            { buffer: Buffer.from(secondImage.data, 'binary'), x: width, y: 0 }], 
+            { width: width * 2, height: height, format: 'jpeg'});
+        await writeFilePromise(join(process.cwd(), `/cat-card.jpg`), result, 'binary');
+        console.log('The file was saved!');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+catCard();
